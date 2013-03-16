9ab7ffdfd9e44a504e14e80bbca92594e0479b8d
Binson Zhang <bin183cs@gmail.com>
2013-03-16 21:47:41 +0800

    Initial CMake project

---
+++ src/toynlp/demo/segmenter_main.cpp
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2013 Binson Zhang.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author	Binson Zhang <bin183cs@gmail.com>
+ * @date		2013-3-16
+ */
+
+#include <iostream>
+#include <glog/logging.h>
+#include <gflags/gflags.h>
+#include <toynlp/seg/segmenter.h>
+
+int main(int argc, char **argv) {
+  FLAGS_stderrthreshold = 0;
+//  FLAGS_log_dir = "log/";
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+
+  VLOG(0) << "------" << argv[0] << "------";
+
+  toynlp::Segmenter seg;
+
+  std::string text = "Hello NLP";
+  std::vector<std::string> tokens;
+  VLOG(0) << "text: " << text;
+  CHECK(seg.Segment(text, &tokens));
+  for (std::size_t i = 0; i < tokens.size(); ++i) {
+    VLOG(0) << "token#" << i << ": " << tokens[i];
+  }
+
+  return 0;
+}
